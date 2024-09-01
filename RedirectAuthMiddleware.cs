548b6faefbee4bc22f739accee402377a14f117d
Siphumelele1954 <162573035+Siphumelele1954@users.noreply.github.com>
2024-09-01 16:38:29 +0200

    Add files via upload

---
+++ RedirectAuthMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace TugwellApp.Middleware
+{
+    public class RedirectAuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RedirectAuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path == "/GuestLanding" && context.Session.GetString("FirebaseToken") != null)
+            {
+                context.Response.Redirect("/Home/Index");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
