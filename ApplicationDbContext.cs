a9c86a8f808c3cf5c09a45a5f5e60647ebad542b
Siphumelele1954 <162573035+Siphumelele1954@users.noreply.github.com>
2024-09-01 16:44:33 +0200

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TugwellApp.Models;
+
+namespace TugwellApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<TugwellApp.Models.Fine> Fine { get; set; } = default!;
+        public DbSet<TugwellApp.Models.Booking> Booking { get; set; } = default!;
+        public DbSet<TugwellApp.Models.DC> DC { get; set; } = default!;
+    }
+}
